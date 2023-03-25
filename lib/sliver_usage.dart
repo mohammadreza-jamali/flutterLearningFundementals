c38bbd1d735d0203f87612b7478cf41f6d5df9a9
mohammad.jamali <mohammad.jamali.marand@gmail.com>
2023-03-25 18:20:02 +0300

    feat:sliver List feat:sliver grid

--- lib/sliver_usage.dart
+++ lib/sliver_usage.dart
@@ -10,23 +10,94 @@ class SliversUsage extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          centerTitle: true,
           title: Text("Colappsable App Bar"),
           expandedHeight: 200,
           floating: true,
           pinned: true,
+          flexibleSpace: FlexibleSpaceBar(background: Image.asset("assets/images/tabriz.jpg",fit: BoxFit.cover,)),
+          snap: true,
         ),
-        _listExample()
-      ],
-
+        SliverPadding(padding: EdgeInsets.all(10),sliver:  _listExample(),),
+        SliverPadding(padding: EdgeInsets.all(20),sliver:  _listWithBuilder(),),
+        _listExtent(), 
+        SliverPadding( padding: EdgeInsets.all(10), sliver: _fixGrid()),
+        SliverPadding( padding: EdgeInsets.all(10), sliver: _builderGrid(),),
+        
+      ]
     );
   }
+
   _listExample(){
-    return SliverFixedExtentList(delegate: SliverChildBuilderDelegate((context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(vertical: 4,horizontal: 2),
-            color: Colors.amber.withBlue(Random().nextInt(255)),
-          );
-        }), itemExtent: 200);
+    return SliverList(delegate: SliverChildListDelegate(
+      [
+        getListItem(1),
+        getListItem(2),
+        getListItem(3),
+        getListItem(4),
+        getListItem(5),
+        getListItem(6),
+      ]
+    ));
+  }
+  
+  _listWithBuilder(){
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: 10,
+        (context, index) => getListItem(index+1)),
+    );
+  }
+  
+  _listExtent(){
+    return SliverFixedExtentList(delegate: SliverChildBuilderDelegate(
+        childCount: 10,
+        (context, index) => getListItem(index+1)), itemExtent: 100);
+  }
+  
+  _fixGrid(){
+    // return SliverGrid(delegate: SliverChildListDelegate(
+    //   [
+    //     getListItem(1),
+    //     getListItem(2),
+    //     getListItem(3),
+    //     getListItem(4),
+    //     getListItem(5),
+    //     getListItem(6),
+    //   ]
+    // ), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3));
+    // return SliverGrid.count(crossAxisCount: 2,children: [
+    //   getListItem(1),
+    //     getListItem(2),
+    //     getListItem(3),
+    //     getListItem(4),
+    //     getListItem(5),
+    //     getListItem(6)
+    //     ]
+    // );
+    return SliverGrid.extent(maxCrossAxisExtent: 100,children: [
+      getListItem(1),
+        getListItem(2),
+        getListItem(3),
+        getListItem(4),
+        getListItem(5),
+        getListItem(6)
+        ]
+    );
+  }
+  _builderGrid(){
+    // return SliverGrid(delegate: SliverChildBuilderDelegate(
+    //         childCount: 10, (context, index) => getListItem(index + 1)),
+    //          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2));
+
+    return SliverGrid.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+    itemCount: 10,
+     itemBuilder:(context,index)=> getListItem(index + 1));
+  }
+
+  getListItem(index){
+    return ListTile(leading: Icon(Icons.add_circle_outline),title: Text("Item $index"),tileColor: createRandomColor(),);
+  }
+  createRandomColor(){
+    return Color.fromARGB(255,Random().nextInt(255) , Random().nextInt(255), Random().nextInt(255));
   }
 }
